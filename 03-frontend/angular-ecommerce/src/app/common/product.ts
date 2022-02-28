27db33ae815b29cee1bc635569c85fb5d9a6f39a
tandersonWakeT <tsanderson2@my.waketech.edu>
2022-02-28 14:19:56 -0500

    begins establishing the angular front end

---
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+
+  sku: string = '';
+  name: string = '';
+  description: string = '';
+  unitPrice: number = 0;
+  imageUrl: string = '';
+  active: boolean = false;
+  unitsInStock: number = 0;
+  dateCreated: Date = new Date();
+  lastUpdated: Date = new Date();
+}

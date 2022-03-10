57e833c3d47f6f8e89b3974a1e5278073d964cbf
tandersonWakeT <tsanderson2@my.waketech.edu>
2022-03-10 17:46:41 -0500

    incorporates Okta login security into the eccommerce app

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+  oidc: {
+    clientId: '0oa447aplcE8erorq5d7',
+    issuer: 'https://dev-28641662.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}

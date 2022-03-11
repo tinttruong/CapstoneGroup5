3cfeec20e0a1ee0e8bcb579ca606e9a563a30377
tandersonWakeT <tsanderson2@my.waketech.edu>
2022-03-11 16:20:19 -0500

    adds user registration to Okta widget

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
 
   oidc: {
-    clientId: '0oa447aplcE8erorq5d7',
-    issuer: 'https://dev-28641662.okta.com/oauth2/default',
+    clientId: '0oa44q9tkh9LVaMTT5d7',
+    issuer: 'https://dev-4187822.okta.com/oauth2/default',
     redirectUri: 'http://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }

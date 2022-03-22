938ec44eb524a58f1cf4be5dd991e6fa2e6fcdaa
tandersonWakeT <tsanderson2@my.waketech.edu>
2022-03-22 13:48:28 -0400

    updates angular front end and okta for https secure communication

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
   oidc: {
     clientId: '0oa44q9tkh9LVaMTT5d7',
     issuer: 'https://dev-4187822.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 }

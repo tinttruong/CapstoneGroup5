b920b0eeb78ddc8d2ab77d575c0f87400b14b9f0
tinttruong <98763015+tinttruong@users.noreply.github.com>
2022-03-25 18:59:01 -0400

    Update environment.ts

--- 03-frontend/angular-ecommerce/src/environments/environment.ts
+++ 03-frontend/angular-ecommerce/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
 
   production: false,
   //compaccesApiUrl: "https://localhost:8443/api",
-  compaccesApiUrl: "https://ecommerce-backend.azurewebsites.net/api",
+  compaccesApiUrl: "https://group5projectbackend.azurewebsites.net/api",
   stripePublishableKey: "pk_test_51KgY2nD8yEo5gUnzHaTsWXAxySFRQCcmuo2kkAtfITaOUGaJOjckV7oTjxnKOXDpcOELkguYDgm87UVHZjiehHsb00pUTH44Jz"
 };
 

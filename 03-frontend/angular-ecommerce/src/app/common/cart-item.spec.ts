624861a66294dabc45d401b5d787f1ed1d5c8c03
tinttruong <98763015+tinttruong@users.noreply.github.com>
2022-04-06 16:12:25 -0400

    Revert "Merge main to Group5Azure"

--- 03-frontend/angular-ecommerce/src/app/common/cart-item.spec.ts
+++ 03-frontend/angular-ecommerce/src/app/common/cart-item.spec.ts
@@ -1,7 +1,13 @@
+import { TestBed } from '@angular/core/testing';
 import { CartItem } from './cart-item';
+import { Product } from './product';
 
 describe('CartItem', () => {
+
+  let productSpy = jasmine.createSpyObj(Product, ['']);
+
   it('should create an instance', () => {
-    expect(new CartItem()).toBeTruthy();
+
+    expect(new CartItem(productSpy)).toBeDefined();
   });
 });
--- 03-frontend/angular-ecommerce/src/app/common/order-item.spec.ts
+++ 03-frontend/angular-ecommerce/src/app/common/order-item.spec.ts
@@ -1,7 +1,10 @@
+import { CartItem } from './cart-item';
 import { OrderItem } from './order-item';
+
+let cartItemSpy = jasmine.createSpyObj(CartItem, ['']);
 
 describe('OrderItem', () => {
   it('should create an instance', () => {
-    expect(new OrderItem()).toBeTruthy();
+    expect(new OrderItem(cartItemSpy)).toBeTruthy();
   });
 });

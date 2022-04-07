b3c728f74b55b944b5e3cb8984ea800efd651d4e
Pranav5xd <49649423+Pranav5xd@users.noreply.github.com>
2022-04-07 11:20:09 -0400

    Inventory Management

--- 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/hcl/ecommerce/service/CheckoutServiceImpl.java
+++ 02-backend/spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/hcl/ecommerce/service/CheckoutServiceImpl.java
@@ -30,25 +30,24 @@ import com.stripe.model.PaymentIntent;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
-	
+
 	private ProductRepository productRepository;
 	private CustomerRepository customerRepository;
 	static Logger log = Logger.getLogger(CheckoutServiceImpl.class);
-	
+
 	@Autowired
 	public CheckoutServiceImpl(CustomerRepository customerRepository, ProductRepository productRepository,
-							   @Value("${stripe.key.secret}") String secretKey) {
+			@Value("${stripe.key.secret}") String secretKey) {
 		this.customerRepository = customerRepository;
 		this.productRepository = productRepository;
 		// initialize stripe API with secret key
 		Stripe.apiKey = secretKey;
 	}
-	
+
 	@Override
 	@Transactional
 	public PurchaseResponse placeOrder(Purchase purchase) {
-		
-		
+
 		// retrieve the order info from dto
 		Order order = purchase.getOrder();
 		log.info("Got order of purchase");
@@ -58,11 +57,12 @@ public class CheckoutServiceImpl implements CheckoutService {
 
 		log.debug("Got order tracking number. Order Tracking number: " + order);
 		// populate order with orderItems
-		
+
 		Set<OrderItem> orderItems = purchase.getOrderItems();
 		orderItems.forEach(item -> order.add(item));
-		for(OrderItem orders:orderItems) {
-			//log.debug("Got order item of id: " +orders.getProductId() + " it has an quantity of " + orders.getQuantity());
+		for (OrderItem orders : orderItems) {
+			// log.debug("Got order item of id: " +orders.getProductId() + " it has an
+			// quantity of " + orders.getQuantity());
 			Optional<Product> orderFromDB = productRepository.findById(orders.getProductId());
 			int decrementAmount = orderFromDB.get().getUnitsInStock() - orders.getQuantity();
 			orderFromDB.get().setUnitsInStock(decrementAmount);
@@ -70,65 +70,60 @@ public class CheckoutServiceImpl implements CheckoutService {
 			Optional<Product> orderFromDB2 = productRepository.findById(orders.getProductId());
 			log.debug(orderFromDB2.get().getUnitsInStock());
 		}
-		
-		
+
 		// populate order with billingAddress and shippingAddress
 		order.setBillingAddress(purchase.getBillingAddress());
 		order.setShippingAddress(purchase.getShippingAddress());
 
 		log.debug("Got order Billing Address: " + purchase.getBillingAddress());
 		log.debug("Got order Shipping Address: " + purchase.getShippingAddress());
-		
+
 		// populate customer with order
 		Customer customer = purchase.getCustomer();
-		
+
 		// check if this is an existing customer
 		String theEmail = customer.getEmail();
-		
+
 		Customer customerFromDB = customerRepository.findByEmail(theEmail);
-		
+
 		if (customerFromDB != null) {
 			// if we find the customer assign to customer variable
 			customer = customerFromDB;
 		}
-		
-		
-		
+
 		customer.add(order);
-		
+
 		// save to the database
 		customerRepository.save(customer);
-		
+
 		// return a response
 		return new PurchaseResponse(orderTrackingNumber);
 	}
-	
+
 	@Override
 	public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
-		
+
 		List<String> paymentMethodTypes = new ArrayList<>();
 		paymentMethodTypes.add("card");
 
 		log.info("Creating payment intent");
 		Map<String, Object> params = new HashMap<>();
-		
-		
+
 		params.put("amount", paymentInfo.getAmount());
 		params.put("currency", paymentInfo.getCurrency());
 		params.put("payment_method_types", paymentMethodTypes);
 		params.put("description", "Computer Accessories purchase");
 		params.put("receipt_email", paymentInfo.getReceiptEmail());
-		
+
 		log.debug("Payment Params: " + params);
-		
+
 		return PaymentIntent.create(params);
 	}
 
 	private String generateOrderTrackingNumber() {
-		
+
 		// generate a random UUID number (UUID version-4)
 		// For details see: Universally unique identifier in wiki
-		
 
 		log.info("Creating random OrderID");
 		return UUID.randomUUID().toString();

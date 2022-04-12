ee4219ae4a62c715875490af3314ed8b47bb5026
Pranav5xd <49649423+Pranav5xd@users.noreply.github.com>
2022-04-12 10:41:21 -0400

    Update my-app-config.ts
    Added support of many users many roles to app config

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -4,6 +4,6 @@ export default {
     clientId: '0oa44q9tkh9LVaMTT5d7',
     issuer: 'https://dev-4187822.okta.com/oauth2/default',
     redirectUri: 'https://group5projectfrontend.azurewebsites.net/login/callback',
-    scopes: ['openid', 'profile', 'email']
+    scopes: ['openid', 'profile', 'email', 'groups']
   }
 }
